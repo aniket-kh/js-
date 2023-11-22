7c338fd49ecf0c81f263fe68a53ee2aed5ac2c33
aniket-kh <107784370+aniket-kh@users.noreply.github.com>
2023-11-22 08:46:29 +0000

    variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail ="aniket@goole.com"
+var accountPassword ="3214"
+accountCity="walhe"
+// accountId=21312 is not allowed beacuase const is contant variable
+accountEmail = "ak@google"
+accountPassword = "7894546"
+accountCity = "pune"
+let accountState;
+// prefer do not ise var keyword beacuase of issue in block chain and function chain
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
