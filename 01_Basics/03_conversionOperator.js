a321e5542d916d11160104b0272f85d1753c1026
aniket-kh <107784370+aniket-kh@users.noreply.github.com>
2023-11-22 10:54:54 +0000

    conversion of operators

---
+++ 01_Basics/03_conversionOperator.js
@@ -0,0 +1,25 @@
+let score = "Aniket"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+//"33adb" => NaN
+// true => 1 ;false =>0
+
+ let isLoggedIn = 33
+ let boolenIsLoggedIn = Boolean(isLoggedIn)
+ console.log(boolenIsLoggedIn);
+
+ //1 = true
+ // false =0
+ // "Aniket" = true
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(someNumber);
+ console.log(typeof stringNumber);
+
