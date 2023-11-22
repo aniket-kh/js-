6170211622ad8dc31a0e0eb19ceb49fe20d0bdfe
aniket-kh <107784370+aniket-kh@users.noreply.github.com>
2023-11-22 09:41:08 +0000

    DataTypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code in newer version
+// alert(3+3) we are using nodejs not a browser
+
+let name= "Aniket"
+let age = 25
+let isLoggedIn= false
+let isDevloper = null
+let happy;
+
+// (1) Premitive DataTypes
+
+// Number = range upto 2 to power 53
+// string
+// boolean
+// undefined
+// BigInt
+// symbol
+// null
+
+
+// (2) Non Premitive DataTypes
+
+// object
+// console.log(typeof "Aniket");
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
